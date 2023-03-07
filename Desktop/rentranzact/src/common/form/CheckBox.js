f2770c386ffce7ab2051757e05b16dc74bcfd967
jnwafor-tme <116190485+jnwafor-tme@users.noreply.github.com>
2023-03-07 15:12:39 +0100

    Delete Desktop directory

--- Desktop/rentranzact/src/common/form/CheckBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CheckBox = () => {
-    return (
-        <div>CheckBox</div>
-    )
-}
-
-export default CheckBox
--- Desktop/rentranzact/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import loginRoutes from '../modules/auth/login/routes'
-import signupRoutes from '../modules/auth/signup/routes'
-
-export default [
-    ...loginRoutes,
-    ...signupRoutes,
-    // ...userRoutes,
-]
